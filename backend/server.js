eaeae97bc4bdb4038a677ad147667e3f91d76554
Abhishek Bhati <bhatiabhishek597@example.com>
2026-01-24 09:20:39 +0530

    git push

--- backend/server.js
+++ backend/server.js
@@ -54,7 +54,9 @@ app.use(compression());
 const allowedOrigins = [
   'https://inplay-two.vercel.app',
   'http://localhost:5173',
-  'http://localhost:3000'
+  'http://localhost:3000',
+  'https://inplays.in',
+  'https://www.inplays.in'
 ];
 
 if (process.env.FRONTEND_URL) {
